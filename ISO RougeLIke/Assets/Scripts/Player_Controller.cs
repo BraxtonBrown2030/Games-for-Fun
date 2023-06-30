0cf706c7d649749a2fafd3925e917d017287cbf6
Braxton Brown <10944268@uvu.edu>
2023-06-29 19:02:42 -0600

    edits and Blocking

--- ISO RougeLIke/Assets/Scripts/Player_Controller.cs
+++ ISO RougeLIke/Assets/Scripts/Player_Controller.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Player_Controller : MonoBehaviour
 {
     
+    [Header("momvment")]
+    private float _Himput;
+    private float _Vimput;
+
+    [SerializeField]
+    private float _Moverspeed;
+    private float _Turnspeed;
+
     void Start()
     {
         
@@ -12,8 +22,17 @@ public class Player_Controller : MonoBehaviour
 
     void Update()
     {
+        Debug.Log("game is running");
 
+        if(Input.GetMouseButton(0))
+        {
+            Attack();
+        }
+    
+    }
+
+    void Attack()
+    {
         
-
     }
 }
