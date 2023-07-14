e41ab2737311b6c21a2327256810a7d473bd0359
Braxton Brown <10944268@uvu.edu>
2023-07-14 04:42:10 -0600

    New project (SP)

---
+++ Space Shooter/Assets/Scripts/Enemys/Fall_Enemy.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Fall_Enemy : MonoBehaviour
+{
+    [Header("Movment")]
+    [SerializeField]private int moveSpeed = -4;
+
+    [Header("Attack")]
+    public int _damage;
+    public float _attackSpeed;
+    public float _lastAttackTime;
+
+    [Header("Health")]
+    public int _totalHeath;
+    public int _curHealth;
+
+    [Header("Game_Objects")]
+    public GameObject _LootDrops;
+    public GameObject _TopEnemy;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        Movement();
+
+    }
+
+    void Movement()
+    {
+
+        transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+
+        if(transform.position.y <= -6.5)
+        {
+
+            float _randomRangeX = Random.Range(-9.5f, 10f);
+
+            transform.position = new Vector3 (_randomRangeX,8,0);
+
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Player")
+        {
+            Player_Controller player = other.transform.GetComponent<Player_Controller>();
+
+            if(player != null)
+            {
+                player.TakeDamage();
+            }
+
+        }
+
+        if(other.tag == "Lazer")
+        {
+
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+
+}
---
+++ Space Shooter/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+[Header("Health")]
+public int _Health;
+
+[Header("Movment")]
+public float _Speed;
+
+[Header("Player Combat")]
+[SerializeField] private float attackSpeed = -0.5f;
+[SerializeField] private float lastAttackTime = 0.3f;
+public int _Damage;
+[SerializeField] GameObject lazer;
+public GameObject[] _player;
+
+
+    void Start()
+    {
+        
+
+
+    }
+
+
+    void Update()
+    {
+        Movment();
+
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > attackSpeed)
+        {
+            Attack();
+        }
+    }
+
+    public void TakeDamage()
+    {
+
+        _Health -= 1;
+
+        if(_Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    void Movment()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * _Speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _Speed * Time.deltaTime);
+
+        if(transform.position.y >= 0)
+         {
+             transform.position = new Vector3(transform.position.x,0,0);
+         }
+        else if(transform.position.y <= -3.8f)
+         {
+             transform.position = new Vector3(transform.position.x, -3.8f, 0);
+         }
+
+        if(transform.position.x >= 11)
+         {
+             transform.position = new Vector3(-11,transform.position.y,0);
+         }
+        else if (transform.position.x <=-11)
+         {
+             transform.position = new Vector3(11,transform.position.y,0);
+         }
+
+    }
+
+    void Attack()
+    {
+        attackSpeed = Time.time + lastAttackTime;
+
+        Debug.Log("working");
+
+    }
+}
