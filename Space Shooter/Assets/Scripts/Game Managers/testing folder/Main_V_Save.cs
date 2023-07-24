a079888ef7721288cc030c4d13e392fdf01df2f3
Braxton Brown <10944268@uvu.edu>
2023-07-23 23:28:44 -0600

    player pfres for volume testing and music slider scripts

---
+++ Space Shooter/Assets/Scripts/Game Managers/testing folder/Main_V_Save.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main_V_Save : MonoBehaviour
+{
+    
+    public AudioSource setVolume;
+    public Slider musicslider;
+    public float musicvolume;
+
+    void Start()
+    {
+        
+        musicvolume = musicslider.value;
+
+    }
+
+    
+    public void Update()
+    {
+        
+        UpdateMusicVolume();
+       // Debug.Log(musicvolume);
+
+    }
+
+    public void UpdateMusicVolume()
+    {
+
+        musicvolume = musicslider.value;
+        PlayerPrefs.SetFloat("music Volume", musicvolume);
+        PlayerPrefs.GetFloat("music Volume", musicvolume);
+
+    }
+}
---
+++ Space Shooter/Assets/Scripts/Game Managers/testing folder/SO_V_Save.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SO_V_Save : ScriptableObject
+{
+    
+    public float savedMusicVolume;
+    public Main_V_Save main_V_Save;
+    
+
+    void Start()
+    {
+        //Main menu saved volume - and refrence
+        main_V_Save = GameObject.FindGameObjectWithTag("Main_V_Save").GetComponent<Main_V_Save>();
+        savedMusicVolume = main_V_Save.musicvolume;
+
+        //Game menu saved vould - Frefence
+
+
+    }
+
+    void Update()
+    {
+        
+        savedMusicVolume = main_V_Save.musicvolume;
+        Debug.Log("SO_V: " + savedMusicVolume);
+
+    }
+}
