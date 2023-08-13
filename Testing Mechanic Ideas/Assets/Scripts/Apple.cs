b1bcbe744fc1aee21c13f56b4eeb51ca6dafbed0
Braxton Brown <10944268@uvu.edu>
2023-08-13 15:05:52 -0600

    (Testing) Game Managers / UI

---
+++ Testing Mechanic Ideas/Assets/Scripts/Apple.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    [Header("Scripts")]
+    [SerializeField] public ScoreManager score;
+    [SerializeField] public SpanwManager spawn;
+
+    [SerializeField] private float degreesPerSecond;
+
+    void Start()
+    {
+
+        score = GameObject.FindGameObjectWithTag("Score Manager").GetComponent<ScoreManager>();
+        spawn = GameObject.FindGameObjectWithTag("Spawn Manager").GetComponent<SpanwManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(new Vector3(0, degreesPerSecond, 0) * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Player")
+        {
+
+            score.IncreaseScore();
+            Destroy(gameObject);
+            spawn.SpanwTimer();
+
+        }
+
+
+    }
+
+}
--- Testing Mechanic Ideas/Assets/Scripts/PlayerHead.cs
+++ Testing Mechanic Ideas/Assets/Scripts/PlayerHead.cs
@@ -33,7 +33,6 @@ public class PlayerHead : MonoBehaviour
 
         }
 
-
     }
 
 }
