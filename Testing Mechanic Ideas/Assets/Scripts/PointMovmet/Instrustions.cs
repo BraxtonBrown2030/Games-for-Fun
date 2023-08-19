75a233607818d94128643d1cfcaaa24a828248b7
Braxton Brown <10944268@uvu.edu>
2023-08-19 09:47:43 -0600

    Ui Game state visual player updates

---
+++ Testing Mechanic Ideas/Assets/Scripts/PointMovmet/Instrustions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Instrustions : MonoBehaviour
+{
+    
+    public TMP_Text placePoints;
+    public TMP_Text gameRunning;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
